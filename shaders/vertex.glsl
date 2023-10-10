faddd0e3505caaa98edff23a58466fc988e4c8b3
Matěj Smetana <matej@smetana.online>
2023-10-10 12:16:04 +0200

    updated shader to support camera

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -5,11 +5,12 @@ layout(location = 0) in vec4 aPos;
 out vec4 color;
 
 uniform mat4 model;
+uniform mat4 view;
 uniform mat4 projection;
 uniform vec3 aColor;
 
 void main() {
-	gl_Position = projection * model * aPos;
+	gl_Position = projection * view * model * aPos;
 
 	color = vec4(aColor, 1.0f);
 }
