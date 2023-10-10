5a4012e97c07985913d74a0f696d74a4e5fb9002
Matěj Smetana <matej@smetana.online>
2023-10-10 12:15:16 +0200

    added factories for tree and temple

--- src/factories.cpp
+++ src/factories.cpp
@@ -1,12 +1,77 @@
 #include "factories.h"
 
-//#include <glm/glm.hpp>
-//#include <SDL2/SDL.h>
-//
-//#include "comps/child.h"
-//#include "comps/position.h"
-//#include "comps/rotation.h"
-//#include "comps/scale.h"
-//#include "comps/transform.h"
-//#include "comps/rotatedByKeyboard.h"
+#include <sstream>
 
+#include "comps/position.h"
+#include "comps/rotation.h"
+#include "comps/scale.h"
+#include "comps/transform.h"
+#include "comps/rotatedByKeyboard.h"
+#include "comps/color.h"
+
+entt::entity factories::createTree(
+	const std::unique_ptr<entt::registry>& registry,
+	const std::unique_ptr<ModelManager>& modelMngr,
+	const std::unique_ptr<ProgramManager>& prgMngr,
+	glm::vec3 pos, glm::vec3 scale
+) {
+	auto tree = registry->create();
+
+	registry->emplace<comps::position>(tree, pos);
+	registry->emplace<comps::rotation>(tree);
+	registry->emplace<comps::scale>(tree, scale);
+	registry->emplace<comps::transform>(tree);
+
+	auto offspring = modelMngr->GenEntities("tree", tree, registry);
+
+	for (const auto& child : *offspring) {
+		registry->emplace<comps::shaderProgram>(child.second, prgMngr->getShaderProgram("noLights"));
+	}
+
+	auto trunk = offspring->at("trunk");
+	registry->emplace<comps::color>(trunk, C_BROWN);
+	auto lowerCone = offspring->at("lower-cone");
+	registry->emplace<comps::color>(lowerCone, C_TREE_GREEN);
+	auto upperCone = offspring->at("upper-cone");
+	registry->emplace<comps::color>(upperCone, C_TREE_GREEN);
+
+	return tree;
+}
+
+entt::entity factories::createTemple(
+	const std::unique_ptr<entt::registry>& registry,
+	const std::unique_ptr<ModelManager>& modelMngr,
+	const std::unique_ptr<ProgramManager>& prgMngr,
+	glm::vec3 pos, glm::vec3 rot, glm::vec3 scale
+) {
+	auto temple = registry->create();
+
+	registry->emplace<comps::position>(temple, pos);
+	registry->emplace<comps::rotation>(temple, rot.y, rot.x, rot.z);
+	registry->emplace<comps::scale>(temple, scale);
+	registry->emplace<comps::transform>(temple);
+
+	auto offspring = modelMngr->GenEntities("temple", temple, registry);
+	
+	for (const auto& child : *offspring) {
+		registry->emplace<comps::shaderProgram>(child.second, prgMngr->getShaderProgram("noLights"));
+	}
+
+	auto platform = offspring->at("platform");
+	registry->emplace<comps::color>(platform, C_STONE2);
+
+	for (int i = 1; i <= 6; i++) {
+		std::stringstream columnName;
+		std::stringstream columnTopName;
+
+		columnName << "column-" << i;
+		columnTopName << "column-top-" << i;
+
+		auto column = offspring->at(columnName.str());
+		registry->emplace<comps::color>(column, C_STONE1);
+		auto columnTop = offspring->at(columnTopName.str());
+		registry->emplace<comps::color>(columnTop, C_STONE1);
+	}
+
+	return temple;
+}
--- src/factories.h
+++ src/factories.h
@@ -1,6 +1,23 @@
 #pragma once
 
 #include <entt/entt.hpp>
+#include <glm/glm.hpp>
+
+#include "modelManager.h"
+#include "programManager.h"
 
 namespace factories {
+	entt::entity createTree(
+		const std::unique_ptr<entt::registry>& registry,
+		const std::unique_ptr<ModelManager>& modelMngr,
+		const std::unique_ptr<ProgramManager>& prgMngr,
+		glm::vec3 pos, glm::vec3 scale
+	);
+
+	entt::entity createTemple(
+		const std::unique_ptr<entt::registry>& registry,
+		const std::unique_ptr<ModelManager>& modelMngr,
+		const std::unique_ptr<ProgramManager>& prgMngr,
+		glm::vec3 pos, glm::vec3 rot, glm::vec3 scale
+	);
 }
