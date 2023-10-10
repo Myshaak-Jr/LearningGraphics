103e33b09c59cd8dfb5f01b691182419f68b05b7
Matěj Smetana <matej@smetana.online>
2023-10-10 12:16:15 +0200

    added colors

--- src/constants.h
+++ src/constants.h
@@ -17,3 +17,9 @@ enum class EAngle {
 #define VEC_FORWARD glm::vec3(0.0f, 0.0f, -1.0f)
 
 #define HEX_COLOR(h) ((((0x##h) >> 16) & 0xff) / 255.0f), ((((0x##h) >> 8) & 0xff) / 255.0f), (((0x##h) & 0xff) / 255.0f)
+
+#define C_TREE_GREEN HEX_COLOR(0a481e)
+#define C_BROWN HEX_COLOR(341a0e)
+
+#define C_STONE1 HEX_COLOR(594f4f)
+#define C_STONE2 HEX_COLOR(4a3c3c)
